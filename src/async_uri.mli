5a27e23b7260d1cce446158a0b4920d8b23c8598
Łukasz Kurowski <crackcomm@gmail.com>
2023-09-30 18:59:40 +0200

    update for async v0.16
    * Module `Time_unix` was replaced by `Time_float_unix`.

--- src/async_uri.mli
+++ src/async_uri.mli
@@ -14,22 +14,22 @@ val connect :
   ?version:Async_ssl.Version.t ->
   ?options:Async_ssl.Opt.t list ->
   ?socket:([ `Unconnected ], Socket.Address.Inet.t) Socket.t ->
-  ?buffer_age_limit:[ `At_most of Time_unix.Span.t | `Unlimited ] ->
+  ?buffer_age_limit:[ `At_most of Time_float_unix.Span.t | `Unlimited ] ->
   ?interrupt:unit Deferred.t ->
   ?reader_buffer_size:int ->
   ?writer_buffer_size:int ->
-  ?timeout:Time_unix.Span.t ->
+  ?timeout:Time_float_unix.Span.t ->
   Uri.t ->
   t Deferred.t
 
 val with_connection :
   ?version:Async_ssl.Version.t ->
   ?options:Async_ssl.Opt.t list ->
-  ?buffer_age_limit:[ `At_most of Time_unix.Span.t | `Unlimited ] ->
+  ?buffer_age_limit:[ `At_most of Time_float_unix.Span.t | `Unlimited ] ->
   ?interrupt:unit Deferred.t ->
   ?reader_buffer_size:int ->
   ?writer_buffer_size:int ->
-  ?timeout:Time_unix.Span.t ->
+  ?timeout:Time_float_unix.Span.t ->
   Uri.t ->
   (t -> 'a Deferred.t) ->
   'a Deferred.t
